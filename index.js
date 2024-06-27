198567d690028ebb8c75716583590461735d0769
FRANK7434 <sonfransiscojames015@gmail.com>
2024-06-27 17:10:33 +0300

    added scope-lap

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName="bob" ;
+var  bestCustomer= undefined;
+const leastFavoriteCustomer="junior"
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+    return  customerName
+}
+function setBestCustomer() {
+    bestCustomer="not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer="maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer ="paul"
+}
